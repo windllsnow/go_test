1df42ef57747ba97ac6c70c335dfaa3eec22817e
wind_flowing <87889501+windllsnow@users.noreply.github.com>
2022-05-15 21:35:47 +0800

    Add files via upload

--- 00001.go
+++ 00001.go
@@ -7,10 +7,29 @@ import (
 	"time"
 )
 
+var foo string = "bar"
+var (
+	x1 bool      = false
+	x2 string    = "info"
+	x3 time.Time = time.Now()
+)
+
 func main() {
 	rand.Seed(time.Now().UnixNano())
 	r := rand.Intn(5) + 1
 	stars := strings.Repeat("*", r)
 	fmt.Println(stars)
 
+	var baz string = "qux"
+	fmt.Println(foo, baz)
+	fmt.Println(x1, x2, x3)
+
+	var seed1 int64 = 1234456789
+
+	rand.Seed(seed1) //重複隨機數  一個位置
+
+	d1 := false
+	l1 := "inter"
+	s1 := time.Now()
+	fmt.Println(d1, l1, s1)
 }
